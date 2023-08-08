aef418d1f5cbc85db5b80d3676519b203eb41375
Triumph <triumphnwabuko29@gmail.com>
2023-08-07 20:12:40 -0700

    cycle check

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,21 +5,21 @@
 
 /**
  * struct listint_s - singly linked list
- * @b: integer
+ * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int b;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int b);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-
-#endif
-
+#endif /* LISTS_H */
