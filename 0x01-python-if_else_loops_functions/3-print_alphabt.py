0c51163ba101e86003586904c781875cbeedd695
Triumph <triumphnwabuko29@gmail.com>
2023-08-08 19:46:19 -0700

    alp

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter == 101:
-        continue
-    elif letter == 113:
-        continue
-    print("{}".format(chr(letter)), end="")
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
+
+
