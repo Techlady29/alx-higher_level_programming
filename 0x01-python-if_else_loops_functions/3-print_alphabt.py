a00787bcfbc244f516530045e290f147a897215c
Triumph <triumphnwabuko29@gmail.com>
2023-08-08 22:12:10 -0700

    alp

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for alp in range(97, 123):
+    if alp == 101:
+        continue
+    elif alp == 113:
+        continue
+    print("{:c}".format(alp), end="")
