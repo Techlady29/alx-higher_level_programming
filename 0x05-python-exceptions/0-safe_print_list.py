9fed1c510242b1aedcc71d6e97de55cf73765812
Techlady29 <triumphnwabuko29@gmail.com>
2023-08-30 17:37:30 +0000

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num = 0
     for j in range(x):
@@ -7,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
             num += 1
         except IndexError:
             break
-    print("")
+    print('')
     return num 
