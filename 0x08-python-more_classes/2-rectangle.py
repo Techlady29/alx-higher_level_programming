ed21f0dfe2d748b65593efda46e0c017c0214f0f
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-05 07:00:46 +0000

    more classes update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
 	@property
 	def width(self):
 		"""this gets the private instance attribute width"""
-	return self.__width
+		return self.__width
 
 	@width.setter
 	def width(self, value):
@@ -26,13 +26,13 @@ class Rectangle:
 	@property
 	def height(self):
         	"""gets the private instance attribute height"""
-	return self.__height
+return self.__height
 
-	@height.setter
-	def height(self, value):
+@height.setter
+def height(self, value):
 	 	"""sets the private instance attribute height"""
-	if type(value) is not int:
-		raise TypeError("the height must be an integer")
+if type(value) is not int:
+raise TypeError("the height must be an integer")
 	if value < 0:
 		raise ValueError("the height must be >= 0")
 	self.__height = value
