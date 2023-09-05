390f8d040d2be45e1249467a4b5d9eb8bd8cc050
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-05 15:49:11 +0000

    next update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 
 """Modules has a function that adds 2 integers."""
 
 def add_integer(a, b=98):
     """Return the addition of two numbers."""
-    if type(a) not int and type(a) not float:
+if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not int and type(b) not float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
