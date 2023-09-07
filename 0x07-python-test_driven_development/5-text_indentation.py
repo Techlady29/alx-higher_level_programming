722446f9e8885a1eb415c9da3e0f439c35d983e9
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-07 21:55:55 +0000

    text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,27 +2,28 @@
 """this defines a text-indentation function"""
 
 def text_indentation(text):
- """Print text with two new lines after each '.', '?', and ':'.
+	"""Print text with two new lines after each '.', '?', and ':'.
 
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+	Args:
+		text (string): The text to print.
+	Raises:
+	TypeError: If text is not a string.
+	"""
 
-    f = 0
-    while f < len(text) and text[f] == ' ':
-        f += 1
+	if not isinstance(text, str):
+		raise TypeError("text must be a string")
 
-    while f < len(text):
-        print(text[f], end="")
-        if text[f] == "\n" or text[f] in ".?:":
-            if text[f] in ".?:":
-                print("\n")
-            f += 1
-            while f < len(text) and text[f] == ' ':
-                f += 1
-            continue
-        f += 1
+	f = 0
+	while f < len(text) and text[f] == ' ':
+		f += 1
+
+	while f < len(text):
+		print(text[f], end="")
+		if text[f] == "\n" or text[f] in ".?:":
+			if text[f] in ".?:":
+				print("\n")
+			f += 1
+			while f < len(text) and text[f] == ' ':
+                		f += 1
+			continue
+		f += 1
