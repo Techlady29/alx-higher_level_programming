4f96c8e2f04097441ad9c16431440c7367627591
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-13 12:03:19 +0000

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this contain the JSON string"""
+
+import json
+
+
+def to_json_string(my_obj):
+	"""this returns the JSON representation of an object (string)"""
+	return json.dumps(my_obj)
