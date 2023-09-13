ef0c2c65fcb1661c22c44bae11e0ce7d9dbe27f8
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-13 11:52:11 +0000

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this contains the function "wrtie_file" """
+
+
+def write_file(filename="", text=""):
+	"""this returns the number of chars written to "filename" from "text" """
+	with open(filename, 'w', encoding='utf=8') as f:
+		return f.write(text)
