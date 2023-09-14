edae82e05c4756f08438e6da1d7dbc040298763c
Techlady29 <triumphnwabuko29@gmail.com>
2023-09-14 04:50:27 +0000

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this defines a class Student"""
+
+
+class Student:
+	"""this represent a student."""
+
+	def __init__(self, first_name, last_name, age):
+		"""this initializes a new Student
+		"""
+		self.first_name = first_name
+		self.last_name = last_name
+		self.age = age
+
+	def to_json(self, attrs=None):
+		"""gets a dictionary representation of the Student
+		If attrs is a list of strings, represents only those attributes
+		included in the list
+		"""
+		if (type(attrs) == list and
+				all(type(ele) == str for ele in attrs)):
+			return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+		return self.__dict__
