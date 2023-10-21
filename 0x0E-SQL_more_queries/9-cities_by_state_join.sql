08eb2b6497ce751c048f5792ceb6ee69e49a03e0
Techlady29 <triumphnwabuko29@gmail.com>
2023-10-21 13:16:32 -0700

    update

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- Records are sorted in order of ascending cities.id.
 SELECT cities.id, cities.name, states.name
 FROM cities 
-INNER JOIN states ON states.id = cities.states_id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
