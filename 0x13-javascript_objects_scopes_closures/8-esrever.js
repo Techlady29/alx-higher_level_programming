b4a1ae1515dc419edde80bb7f477d93b08cf287f
Techlady29 <triumphnwabuko29@gmail.com>
2023-11-18 08:46:00 -0800

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-export.esrever = function (list) {
+exports.esrever = function (list) {
 	const newList = [];
 	for (let i = list.length - 1; i >= 0; i--) {
 		newList.push(list[i]);
